fec7bab671aeb685c8ced66415eb0b69b7bc1a81
henryRoby <razanabelahyhenri@gmail.com>
2019-07-27 17:05:34 +0300

    modification2

--- src/components/newAtelier/NewAtelier.js
+++ src/components/newAtelier/NewAtelier.js
@@ -3,11 +3,11 @@ import React from 'react';
 import './new.css'
 
 class NewAtelier extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       titre: '',
-      utilisateur:'',
+      utilisateur: '',
       prix: '',
       debut: '',
       duree: '',
@@ -18,48 +18,49 @@ class NewAtelier extends React.Component {
     }
     this.onChange = this.onChange.bind(this)
     this.handleUploadImage = this.handleUploadImage.bind(this);
-}
+  }
 
-onChange(event) {
-  this.setState({
-    [event.target.name]: event.target.value
-  })
-}
-handleUploadImage(ev) {
-  ev.preventDefault();
+  onChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  handleUploadImage(ev) {
+    ev.preventDefault();
 
-  const data = new FormData();
-  data.append('image', this.uploadInput.files[0]);
-  data.append('titre', this.state.titre);
-  data.append('prix', this.state.prix);
-  data.append('debut', this.state.debut);
-  data.append('duree', this.state.duree);
-  data.append('placeRes', 0);
-  data.append('place', this.state.place);
-  data.append('idUser', localStorage.id);
-  data.append('description', this.state.description)
+    const data = new FormData();
+    data.append('image', this.uploadInput.files[0]);
+    data.append('titre', this.state.titre);
+    data.append('prix', this.state.prix);
+    data.append('debut', this.state.debut);
+    data.append('duree', this.state.duree);
+    data.append('placeRes', 0);
+    data.append('place', this.state.place);
+    data.append('idUser', localStorage.id);
+    data.append('description', this.state.description)
 
-  fetch('https://henrikely.herokuapp.com/api/users/newArticle/', {
-    method: 'POST',
-    body: data,
-  }).then((response) => {
-    response.json().then((body) => {
-      this.setState({ image: `https://henrikely.herokuapp.com/api/users/newArticle/${body.image}` });
-      console.log('ity ilay body.image', body.image);
+    fetch('https://henrikely.herokuapp.com/api/users/newArticle/', {
+      method: 'POST',
+      body: data,
+    }).then((response) => {
+      response.json().then((body) => {
+        this.setState({ image: `https://henrikely.herokuapp.com/api/users/newArticle/${body.image}` });
+        console.log('ity ilay body.image', body.image);
 
+      });
     });
-  });
-}
+  }
 
   render() {
     return (
       <div className="container-fluid">
         <form onSubmit={this.handleUploadImage} className="md-form">
           <div className="form-group mx-sm-3 mb-2" id="ajoutatelier">
+
             <div className="row" >
               <div className="col-md-6">
 
-              <input className="form-control" type="text"
+                <input className="form-control" type="text"
                   value={this.state.value}
                   onChange={this.onChange}
                   name="titre" placeholder="Titre" />
@@ -67,71 +68,78 @@ handleUploadImage(ev) {
               </div>
               <div className="col-md-6">
 
-              <input className="form-control" type="text"
-                  value={this.state.value}
-                  onChange={this.onChange}
-                  name="prix" placeholder="Prix" />
-                
-              </div>
-            </div>
-            <br />
-            <br />
-            <div className="row">
-              <div className="col-md-6">
-              <input className="form-control" type="time"
-                  value={this.state.value}
-                  onChange={this.onChange}
-                  name="debut" placeholder="Debut" />
-              </div>
-
-              <div className="col-md-6">
-
                 <input className="form-control" type="text"
                   value={this.state.value}
                   onChange={this.onChange}
-                  name="description" placeholder="Description" /><br/>
+                  name="prix" placeholder="Prix" />
 
               </div>
             </div>
-           
-            <br />
-            <div className="row">
-              <div className="col-md-6">
-              <input className="form-control" type="number"
-                  value={this.state.value}
-                  onChange={this.onChange}
-                  name="place" placeholder="Nombre des places" />
-              </div>
-              <div className="col-md-6">
-                <input className="form-control" type="time"
-                  value={this.state.value}
-                  onChange={this.onChange}
-                  name="duree" placeholder="Durée" />
-              </div>
-            </div>
-            <br />
-            <div className="row">
-              <div className="col-md-6">
-              <input className="form-control" type="date"
-                  value={this.state.value}
-                  onChange={this.onChange}
-                  name="date" placeholder="Date" />
-              </div>
-              <div className="col-md-6">
-                
-              </div>
-            </div>
-            <br />
-        
-            <div className="row">
-            <input ref={(ref) => { this.uploadInput = ref; }} id="fichier" type="file" name="image" />
-                <button id="validate" className="btn btn-info">Publier</button>
-             
-              
-            </div>
-            
-            
+
           </div>
+          <br />
+          <br />
+          <div className="row">
+            <div className="col-md-6">
+              <input className="form-control" type="time"
+                value={this.state.value}
+                onChange={this.onChange}
+                name="debut" placeholder="Debut" />
+            </div>
+
+            <div className="col-md-6">
+
+              <input className="form-control" type="time"
+                value={this.state.value}
+                onChange={this.onChange}
+                name="duree" placeholder="Duree" /><br />
+
+            </div>
+          </div>
+
+          <br />
+          <div className="row">
+            <div className="col-md-6">
+              <input className="form-control" type="number"
+                value={this.state.value}
+                onChange={this.onChange}
+                name="place" placeholder="Nombre des places" />
+            </div>
+            <div className="col-md-6">
+              <input className="form-control" type="time"
+                value={this.state.value}
+                onChange={this.onChange}
+                name="placeRes" placeholder="number" />
+            </div>
+          </div>
+          <br />
+          <div className="row">
+            <div className="col-md-6">
+              <input className="form-control" type="date"
+                value={this.state.value}
+                onChange={this.onChange}
+                name="date" placeholder="Date" />
+            </div>
+            <div className="col-md-6">
+            <div className="col-md-6">
+              <input className="form-control" type="date"
+                value={this.state.value}
+                onChange={this.onChange}
+                name="description" placeholder="Description" />
+            </div>
+            </div>
+          </div>
+          <br />
+
+          <div className="row">
+            <input ref={(ref) => { this.uploadInput = ref; }} id="fichier" type="file" name="image" />
+            <button id="validate" className="btn btn-info">Publier</button>
+
+
+          </div>
+
+
+
 
         </form>
       </div>
